1e3fa32657e3caac0165fc65cac30c449e08a51f
Faber <john_estrada23222@elpoli.edu.co>
2025-06-18 13:02:46 -0500

    inicio de proyecto

---
+++ src/main/java/com/example/examen/ExamenFinalCes3Application.java
@@ -0,0 +1,13 @@
+package com.example.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalCes3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalCes3Application.class, args);
+	}
+
+}
